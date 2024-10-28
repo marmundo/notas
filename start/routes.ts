b302d009e4c5a533eca84f63de686bcb4a497ba6
Marcelo Damasceno <marcelo.melo@nees.ufal.br>
2024-10-28 11:53:28 -0300

    refactor: Adicionar nome do usuário nas notas retornadas pela API
    Adicionei a funcionalidade de incluir o nome do usuário nas notas retornadas pela API. Agora, ao buscar as notas, o nome do usuário associado a cada nota será exibido. Isso permite uma melhor visualização das informações e facilita a identificação do autor de cada nota.

--- start/routes.ts
+++ start/routes.ts
@@ -9,40 +9,77 @@
 
 import router from '@adonisjs/core/services/router'
 
-let notasDatabase = {
-  notas: [
-    {
-      id: 1,
-      titulo: 'Nota 1',
-      descricao: 'Descrição da nota 1',
-    },
-    {
-      id: 2,
-      titulo: 'Nota 2',
-      descricao: 'Descrição da nota 2',
-    },
-  ],
+interface Nota {
+  id: number
+  titulo: string
+  descricao: string
+  usuario: string | number
 }
+
+interface Usuario {
+  id: number
+  nome: string
+}
+
+let usuariosDatabase: Usuario[] = [
+  {
+    id: 1,
+    nome: 'Usuário 1',
+  },
+  {
+    id: 2,
+    nome: 'Usuário 2',
+  },
+]
+
+let notasDatabase: Nota[] = [
+  {
+    id: 1,
+    titulo: 'Nota 1',
+    descricao: 'Descrição da nota 1',
+    usuario: 1,
+  },
+  {
+    id: 2,
+    titulo: 'Nota 2',
+    descricao: 'Descrição da nota 2',
+    usuario: 2,
+  },
+]
 router.get('/', async () => {
-  return notasDatabase.notas
+  return getNotasComNomeUsuario(notasDatabase)
 })
+
+function getNotasComNomeUsuario(notas: Nota[]): Nota[] {
+  let notasComUsuario = notas.map((nota: Nota) => {
+    const usuarioEncontrado = usuariosDatabase.find((usuario) => usuario.id === nota.usuario)
+    return {
+      ...nota,
+      usuario: usuarioEncontrado ? usuarioEncontrado.nome : 'Usuário não encontrado',
+    }
+  })
+  return notasComUsuario
+}
 
 router.get('/notas', async ({ request }) => {
   const { titulo, descricao } = request.qs()
   if (titulo && descricao) {
-    return notasDatabase.notas.filter(
+    let notasFiltradas = notasDatabase.filter(
       (nota) => nota.titulo.includes(titulo) && nota.descricao.includes(descricao)
     )
+    return getNotasComNomeUsuario(notasFiltradas)
   } else if (titulo) {
-    return notasDatabase.notas.filter((nota) => nota.titulo.includes(titulo))
+    return getNotasComNomeUsuario(notasDatabase.filter((nota) => nota.titulo.includes(titulo)))
   } else if (descricao) {
-    return notasDatabase.notas.filter((nota) => nota.descricao.includes(descricao))
+    return getNotasComNomeUsuario(
+      notasDatabase.filter((nota) => nota.descricao.includes(descricao))
+    )
   } else {
-    return notasDatabase.notas
+    return getNotasComNomeUsuario(notasDatabase)
   }
 })
 
 router.get('/:id', async ({ params }) => {
   const id = Number.parseInt(params.id)
-  return notasDatabase.notas.find((nota) => nota.id === id)
+  return notasDatabase.find((nota) => nota.id === id)
 })
