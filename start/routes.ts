3ddac36471bb67b791be9728d9655ae57e480215
Marcelo Damasceno <marcelo.melo@nees.ufal.br>
2024-10-28 11:28:42 -0300

    feat: Adicionar rotas para busca de notas por título, descrição e ID
    Adicionei rotas para buscar notas por título, descrição e ID. Agora é possível fazer consultas mais específicas na API de notas.

--- start/routes.ts
+++ start/routes.ts
@@ -9,8 +9,40 @@
 
 import router from '@adonisjs/core/services/router'
 
+let notasDatabase = {
+  notas: [
+    {
+      id: 1,
+      titulo: 'Nota 1',
+      descricao: 'Descrição da nota 1',
+    },
+    {
+      id: 2,
+      titulo: 'Nota 2',
+      descricao: 'Descrição da nota 2',
+    },
+  ],
+}
 router.get('/', async () => {
-  return {
-    hello: 'world',
+  return notasDatabase.notas
+})
+
+router.get('/notas', async ({ request }) => {
+  const { titulo, descricao } = request.qs()
+  if (titulo && descricao) {
+    return notasDatabase.notas.filter(
+      (nota) => nota.titulo.includes(titulo) && nota.descricao.includes(descricao)
+    )
+  } else if (titulo) {
+    return notasDatabase.notas.filter((nota) => nota.titulo.includes(titulo))
+  } else if (descricao) {
+    return notasDatabase.notas.filter((nota) => nota.descricao.includes(descricao))
+  } else {
+    return notasDatabase.notas
   }
+})
+
+router.get('/:id', async ({ params }) => {
+  const id = Number.parseInt(params.id)
+  return notasDatabase.notas.find((nota) => nota.id === id)
 })
