c2cd5250dfb4dd812f1443ff05f560d64966b7a3
Marcelo Damasceno <marcelo.melo@nees.ufal.br>
2024-10-29 22:40:57 -0300

    chore: Remove commented code in test setup
    The commented code in the test setup was removed to improve code readability and maintainability.

--- tests/bootstrap.ts
+++ tests/bootstrap.ts
@@ -23,7 +23,8 @@ export const plugins: Config['plugins'] = [assert(), apiClient(), pluginAdonisJS
  * The teardown functions are executed after all the tests
  */
 export const runnerHooks: Required<Pick<Config, 'setup' | 'teardown'>> = {
-  setup: [() => testUtils.db().migrate()],
+  setup: [],
+  // setup: [() => testUtils.db().migrate()],
   teardown: [],
 }
 
