d534ee231624158b23218e80351b3949b385bd84
Marcelo Damasceno <marcelodamasceno@gmail.com>
2024-10-31 17:00:03 -0300

    Add functional tests for notas endpoints

---
+++ tests/functional/notas/notas.spec.ts
@@ -0,0 +1,19 @@
+import { test } from '@japa/runner'
+
+test.group('Notas notas', () => {
+  test('deveria acessar o google com sucesso', async ({ client }) => {
+    const resposta=await client.get('https://www.google.com')
+    resposta.assertStatus(200)
+    // resposta.dump()
+  })
+
+  test('deveria listar todas as notas', async ({ client }) => {
+    const resposta=await client.get('/notas')
+    resposta.assertStatus(200)
+  })
+
+  test('deveria dar erro se a rota nao existe', async ({ client }) => {
+    const resposta=await client.get('/pedroDavi')
+    resposta.assertStatus(404)
+  })
+})
