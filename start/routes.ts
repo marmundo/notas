c91df3ac51d2eedc399c845228f9233ea6b17b7d
Marcelo Damasceno <marcelodamasceno@gmail.com>
2024-11-06 17:39:09 -0300

    Implement index method in NotasController to return a list of notes and set up resource routes for notas

--- start/routes.ts
+++ start/routes.ts
@@ -8,35 +8,37 @@
 */
 
 import router from '@adonisjs/core/services/router'
+const NotasController = () => import('#controllers/notas_controller')
 
-router.get('/', async () => {
-  return {
-    marcelo: 'goat',
-  }
-})
+// router.get('/', async () => {
+//   return {
+//     marcelo: 'goat',
+//   }
+// })
 
-let notasDatabase=[
-  {id:1,titulo:'nota 1',conteudo:'nota 1',autor:'marcelo'},
-  {id:2,titulo:'nota 2',conteudo:'nota 1',autor:'marcelo'},
-  {id:3,titulo:'nota 3',conteudo:'nota 1',autor:'marcelo'},
-]
+// let notasDatabase=[
+//   {id:1,titulo:'nota 1',conteudo:'nota 1',autor:'marcelo'},
+//   {id:2,titulo:'nota 2',conteudo:'nota 1',autor:'marcelo'},
+//   {id:3,titulo:'nota 3',conteudo:'nota 1',autor:'marcelo'},
+// ]
 
-router.get('/notas',async({request})=>{
-  const id = request.input('id') 
-  if(id)
-    return notasDatabase.find(nota => nota.id == id)
-  return notasDatabase
-})
-// rota com query string
-router.get('/notas/:id',async({params})=>{
-  return notasDatabase.find(nota=>nota.id==params.id)})
+// router.get('/notas',async({request})=>{
+//   const id = request.input('id') 
+//   if(id)
+//     return notasDatabase.find(nota => nota.id == id)
+//   return notasDatabase
+// })
+// // rota com query string
+// router.get('/notas/:id',async({params})=>{
+//   return notasDatabase.find(nota=>nota.id==params.id)})
 
-router.post('/notas',async({request,response})=>{
-  //body={titulo:'nota 4',conteudo:'nota 4',autor:'marcelo'}
-  const nota=request.body()
-  nota.id=notasDatabase.length+1
-  notasDatabase.push(nota)
-  response.status(201)
-  return nota
-})
+// router.post('/notas',async({request,response})=>{
+//   //body={titulo:'nota 4',conteudo:'nota 4',autor:'marcelo'}
+//   const nota=request.body()
+//   nota.id=notasDatabase.length+1
+//   notasDatabase.push(nota)
+//   response.status(201)
+//   return nota
+// })
 
+router.resource('notas', NotasController).apiOnly()
